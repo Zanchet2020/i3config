626c05292002793cee4d3ac01907a5012cef8bd2
Zanchet2020 <lucasamuel040@gmail.com>
2024-05-31 10:16:30 -0300

    chenged startup apps

--- autostart.sh
+++ autostart.sh
@@ -5,6 +5,6 @@ i3-msg 'workspace 1; exec emacs-gtk+x11'
 sleep 1
 i3-msg 'workspace 2; exec microsoft-edge'
 sleep 1
-i3-msg 'workspace 3; exec whatsapp-for-linux'
-sleep 1
+#i3-msg 'workspace 3; exec ferdium'
+#sleep 1
 i3-msg 'workspace 1'
